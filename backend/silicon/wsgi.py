dc9ebcda143122e7e2b03cd57c3a6b2e4a32ba89
David Makinde <58576209+daevid-thegreat@users.noreply.github.com>
2024-04-11 20:55:23 +0100

    updated settings config and created model class for chat

--- backend/silicon/wsgi.py
+++ backend/silicon/wsgi.py
@@ -10,7 +10,19 @@ https://docs.djangoproject.com/en/4.2/howto/deployment/wsgi/
 import os
 
 from django.core.wsgi import get_wsgi_application
+import environ
+from pathlib import Path
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'silicon.settings.production')
+
+env = environ.Env(DEBUG=(bool, False))
+# Build paths inside the project like this: BASE_DIR / 'subdir'.
+BASE_DIR = Path(__file__).resolve().parent.parent
+
+# Take environment variables from .env file
+environ.Env.read_env(os.path.join(BASE_DIR, '.env'))
+
+environment = env('ENVIRONMENT')
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'silicon.settings.' + environment)
 
 application = get_wsgi_application()
