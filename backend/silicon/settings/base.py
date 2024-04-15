7a177f52c49f34c3d34577b633176911c5325f38
The Raven <raven.igwesi@gmail.com>
2024-04-15 21:44:35 +0100

    Updated settings

--- backend/silicon/settings/base.py
+++ backend/silicon/settings/base.py
@@ -16,7 +16,7 @@ environ.Env.read_env(os.path.join(BASE_DIR, '.env'))
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = env('SECRET_KEY')
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 # Application definition
 DJANGO_APPS = [
--- backend/silicon/urls.py
+++ backend/silicon/urls.py
@@ -1,7 +1,12 @@
 from django.contrib import admin
 from django.urls import path, include
+from django.http import JsonResponse
+
+def api(request):
+    return JsonResponse({'msg': 'API LIVE', 'status': 200})
 
 urlpatterns = [
+    path('', api, name="api"),
     path('api/user/', include('apps.accounts.api.urls')),
     path('api/finance/', include('apps.Finance.api.urls')),
     path('api/chat/', include('apps.chat.urls')),
